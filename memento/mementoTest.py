bcce2c01f9613437f0b5e393cee4eaa5c37b8811
BrunoDM2943 <bdm2943@gmail.com>
2016-04-21 23:06:10 -0300

    Memento Pattern started

---
+++ memento/mementoTest.py
@@ -0,0 +1,20 @@
+import unittest
+from memento.saves import SaveFile, MemoryCard
+
+__author__ = 'Bruno'
+
+
+class Test(unittest.TestCase):
+
+    def test_save_file(self):
+        save1 = SaveFile()
+        m = MemoryCard()
+        self.assertTrue(save1.name, 'save_file');
+        m.add_save(save1.save_file())
+        save1.name = 'bla'
+        self.assertTrue(save1.name, 'bla');
+        save1.return_save(m.get_save(0))
+        self.assertTrue(save1.name, 'save_file');
+
+if __name__ == '__main__':
+    unittest.main()
