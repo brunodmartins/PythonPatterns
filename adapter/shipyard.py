122b7221a75ad12b8b88a6ec5bfd5cb9ea33442e
BrunoDM2943 <bdm2943@gmail.com>
2016-04-26 14:07:20 -0300

    Adapter Impl

--- adapter/shipyard.py
+++ adapter/shipyard.py
@@ -5,30 +5,54 @@ Created on 26/04/2016
 '''
 import abc
 
-class Captain(object):
-    '''
-    classdocs
-    '''
-
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
-
 class Battleship(object):
     '''
     classdocs
     '''
     __metaclass__ = abc.ABCMeta;
+    
+    def fire(self):
+        raise NotImplementedError
+    
+    def move(self):
+        raise NotImplementedError
 
-
-class BattleFishingBoat(object):
+class Captain(Battleship):
     '''
     classdocs
     '''
+
+    def __init__(self, battleship=None):
+        '''
+        Constructor
+        '''
+        self.battleship = battleship
+
+    def fire(self):
+        print 'Fire'
+    
+    def move(self):
+        print 'Move'
+
+class BattleFishingBoat(Battleship):
+    '''
+    classdocs
+    '''
+    
+    def fire(self):
+        print 'Fire'
+    
+    def move(self):
+        print 'Move'
 
 class FishihingBoat(object):
     '''
     classdocs
     '''
+    
+    def sail(self):
+        print 'Sail'
+    
+    def fish(self):
+        print 'Fish'
         
