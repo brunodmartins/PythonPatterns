e90945295ce5cbdd1b97b3dcdcb8eabdabc86132
BrunoDM2943 <bdm2943@gmail.com>
2016-04-26 13:59:53 -0300

    Adapter pattern started

---
+++ adapter/shipyard.py
@@ -0,0 +1,34 @@
+'''
+Created on 26/04/2016
+
+@author: bruno.martins
+'''
+import abc
+
+class Captain(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+
+class Battleship(object):
+    '''
+    classdocs
+    '''
+    __metaclass__ = abc.ABCMeta;
+
+
+class BattleFishingBoat(object):
+    '''
+    classdocs
+    '''
+
+class FishihingBoat(object):
+    '''
+    classdocs
+    '''
+        
