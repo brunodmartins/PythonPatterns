6921214eac312e0f39a61da84184f22795790bb4
BrunoDM2943 <bdm2943@gmail.com>
2016-05-12 19:55:38 -0300

    composite

--- bridge/computer.py
+++ bridge/computer.py
@@ -5,45 +5,81 @@ Created on 10/05/2016
 @author: bruno.martins
 '''
 
+
 class Processor(object):
+    """
+    Generic CPU
+    """
     __metaclass__ = abc.ABCMeta
-    '''
-    classdocs
-    '''
+
     def __init__(self, alu=None):
-        '''
+        """
         Constructor
-        '''
+        """
         self.alu = alu
-    
-    def sumTwoNumbers(self,number1, number2):
-        print(self.alu.sum(number1,number2)) 
 
-class AMDProcessor(Processor):
-    '''
-    classdocs
-    '''
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
-
-class ALU(object):
-    __metaclass__ = abc.ABCMeta
-    
-    
-    def __init__(self):
-        '''
-        Constructor
-        '''
-    @abc.abstractmethod
-    def sum(self,number1,number2):
+    def sum_two_numbers(self, number1, number2):
+        """
+        Sum two numbers
+        """
         raise NotImplementedError
 
-class AMDALU(object):
-    __metaclass__ = abc.ABCMeta
-    
-    def __init__(self):
-        '''
+
+class AMDProcessor(Processor):
+    """
+    AMD CPU
+    """
+
+    def __init__(self, alu):
+        """
         Constructor
-        '''
+        """
+        super().__init__(alu)
+
+    def sum_two_numbers(self, number1, number2):
+        print(self.alu.sum(number1, number2))
+
+
+class Alu(object):
+    """
+    Generic ALU
+    """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+    @abc.abstractmethod
+    def sum(self, number1, number2):
+        """
+        Sum two number
+        """
+        raise NotImplementedError
+
+
+class AmdAlu(Alu):
+    """
+    AMD ALU
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        super().__init__()
+
+    def sum(self, number1, number2):
+        print('AMD: {} + {} = {}'.format(number1, number2, number1 + number2))
+        return number1 + number2
+
+
+class IntelAlu(Alu):
+    """
+    Intel ALU
+    """
+
+    def sum(self, number1, number2):
+        print('Intel: {} + {} = {}'.format(number1, number2, number1 + number2))
+        return number1 + number2
