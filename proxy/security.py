8a1cfb1e8b4a9e0c514648881fbf538c7ceafb4c
BrunoDM2943 <bdm2943@gmail.com>
2016-08-05 21:13:42 -0300

    Adding some comments

--- proxy/security.py
+++ proxy/security.py
@@ -1,30 +1,59 @@
-'''
+"""
 Created on 04/08/2016
 
 @author: bruno.martins
-'''
+"""
 import abc
 
+
 class ISecurityLogin:
+    """
+    ISecurityLogin interface
+    """
     __metaclass__ = abc.ABCMeta
     
     @abc.abstractmethod
     def login(self, name, pw, permission):
+        """
+        Do login
+        :param name: User's name
+        :param pw:  User's password
+        :param permission:  User's permission
+        """
         raise NotImplementedError
     
 
 class SecurityLogin(ISecurityLogin):
-    
+    """
+    SecurityLogin class implementing ISecurityLogin
+    """
+
     def login(self, name, pw, permission):
-        print("Hello {}, you logged sucessfully!".format(name))
+        """
+        Do login
+        :param name: User's name
+        :param pw:  User's password
+        :param permission:  User's permission
+        """
+        print("Hello {}, you logged successfully!".format(name))
+
 
 class SecurityLoginProxy(ISecurityLogin):
+    """
+    SecurityLogin's Proxy
+    """
     
     securityLogin = SecurityLogin()
     
     def login(self, name, pw, permission=""):
+        """
+        Verify if a given user has permission to
+        precede
+        :param name: User's name
+        :param pw:  User's password
+        :param permission:  User's permission
+        """
         if permission == "":
             raise Exception('Not authorized')
         else:
             self.securityLogin.login(name, pw, permission)
-        
--- proxy/test_proxy.py
+++ proxy/test_proxy.py
@@ -1,24 +1,22 @@
-'''
+"""
 Created on 04/08/2016
 
 @author: bruno.martins
-'''
+"""
 import unittest
 from proxy import security
 
 
 class Test(unittest.TestCase):
 
-
     def testProxy(self):
-        securityLogin = security.SecurityLoginProxy()
-        securityLogin.login('Bruno', '123456', 'ADM')
+        security_login = security.SecurityLoginProxy()
+        security_login.login('Bruno', '123456', 'ADM')
         try:
-            securityLogin.login('Bruno', '123456')
+            security_login.login('Bruno', '123456')
         except Exception as e:
             print(e)
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testProxy']
     unittest.main()
