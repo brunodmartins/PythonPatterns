10469d8cb2c84b06c0b108dfe4157e370f273241
BrunoDM2943 <bdm2943@gmail.com>
2016-09-28 21:02:42 -0300

    PEP8

--- interpreter/Calculator.py
+++ interpreter/Calculator.py
@@ -1,33 +1,69 @@
+"""
+Calculator module
+"""
 import abc
 
 __author__ = 'Bruno'
 
 
 class Context:
+    """
+    Context'class
+    """
+
     def __init__(self, input_text):
+        """
+        Constructor
+        """
         self.input_text = input_text
 
 
 class Expression:
+    """
+    Expression's class
+    """
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
     def interpret(self, context):
+        """
+        Interpret the Expression
+        """
         raise NotImplementedError
 
 
 class NumberExpression(Expression):
+    """
+    NumberExpression'class
+    """
+
     def __init__(self, number):
+        """
+        Constructor
+        """
         self.number = number
 
     def interpret(self, context):
+        """
+        Interpret the Expression
+        """
         return self.number
 
 
 class PlusExpression(Expression):
+    """
+    PlusExpression's class
+    """
+
     def __init__(self, left, right):
+        """
+        Constructor
+        """
         self.left = left
         self.right = right
 
     def interpret(self, context):
+        """
+        Interpret the Expression
+        """
         return self.left.interpret(context) + self.right.interpret(context)
--- iterator/test_iterator.py
+++ iterator/test_iterator.py
@@ -4,19 +4,18 @@ Created on 04/08/2016
 @author: bruno.martins
 '''
 import unittest
-from ford import *
+from ford import Fabric, Car
 
 
 class Test(unittest.TestCase):
 
-
-    def testIterator(self):
+    def test_iterator(self):
         fabric = Fabric()
         fabric.cars.append(Car("Focus", "Black"))
         fabric.cars.append(Car("Ka", "Red"))
         fabric.cars.append(Car("Mustang", "Green"))
         iterator = fabric.getIterator()
-        while(iterator.hasNext()):
+        while iterator.hasNext():
             print(iterator.next())
 
 
